f8b222e4cd1bc97cd2f1eaefba8c4c993843966c
João Augusto <jaugustovpires@gmail.com>
2023-12-14 09:21:49 -0300

    fix: height in about section

--- frontend/src/pages/sections/About.jsx
+++ frontend/src/pages/sections/About.jsx
@@ -10,9 +10,9 @@ export const About = () => {
   return (
     <section
       id="home"
-      className="bg-contain w-full h-[1350px] lg:h-[1000px] flex flex-col items-center justify-center bg-gray-1 text-gray-4 mt-16"
+      className="bg-contain w-full h-[1350px] md:h-[1200px] lg:h-[1000px] flex flex-col items-center justify-center bg-gray-1 text-gray-4 mt-16"
     >
-      <div className="w-full max-w-7xl *max-h-screen* flex items-center justify-center lg:justify-between p-8">
+      <div className="w-full max-w-7xl flex items-center justify-center lg:justify-between p-8">
         <div className="w-full max-w-lg flex flex-col gap-10">
           <div className="w-full flex flex-col gap-4">
             <h1 className="text-4xl font-bold">
